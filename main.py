a27c6534fdf411ac6858aa4ae9e3068ef7604d50
Irtaza Faisal <irtazafaisal01@gmail.com>
2023-06-08 00:19:28 +0500

    Make Basic Player and Controller

---
+++ main.py
@@ -0,0 +1,33 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+run = True
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+player = pygame.Rect((300, 250, 50, 50))
+
+
+while run:
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen, (0, 255, 0), player)
+    key = pygame.key.get_pressed()
+
+    if key[pygame.K_a]:
+        player.move_ip(-1,0)
+    elif key[pygame.K_d]:
+        player.move_ip(1,0)
+    elif key[pygame.K_s]:
+        player.move_ip(0,1)
+    elif key[pygame.K_w]:
+        player.move_ip(0,-1)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
